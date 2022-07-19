d9f7b10bf64ddc139082b591c7a4d1237f3b1892
Adit0507 <adi05072002@gmail.com>
2022-07-19 18:36:31 +0530

    Finished the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="holder">
+      <Navbar />
+      <div className="header-content flex flex-c text-center text-white">
+        <h2 className="header-title text-capitalize">
+          find your book of choice.
+        </h2>
+        <br />
+        <p className="header-text fs-18 fw-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
+          sapiente quibusdam consequatur perspiciatis facere laboriosam non
+          nesciunt at id repudiandae, modi iste? Eligendi, rerum!
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
