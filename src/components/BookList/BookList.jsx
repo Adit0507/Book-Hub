547fc7f1fad5d17c2cb6c49d3175112515f8dab7
Adit0507 <adi05072002@gmail.com>
2022-07-19 20:25:35 +0530

    Minor fixes

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -5,14 +5,11 @@ import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./BookList.css";
 
-//https://covers.openlibrary.org/b/id/240727-S.jpg
-
 const BookList = () => {
   const {books, loading, resultTitle} = useGlobalContext();
   const booksWithCovers = books.map((singleBook) => {
     return {
       ...singleBook,
-      // removing /works/ to get only id
       id: (singleBook.id).replace("/works/", ""),
       cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : coverImg
     }
