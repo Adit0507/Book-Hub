730a04ffa6054e44979b10cd79c25b22952eeb89
Adit0507 <adi05072002@gmail.com>
2022-07-19 18:29:03 +0530

    Finished the Search Form component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../context.";
+import "./SearchForm.css";
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
+  const searchText = useRef("");
+  const navigate = useNavigate();
 
-export default SearchForm
+  useEffect(() => searchText.current.focus(), []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempSearchTerm = searchText.current.value.trim();
+    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length === 0) {
+      setSearchTerm("To Pimp A Butterfly");
+      setResultTitle("Please Enter Something ...");
+    } else {
+      setSearchTerm(searchText.current.value);
+    }
+
+    navigate("/book");
+  };
+
+  return (
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="To Pimp A Butterfly"
+                ref={searchText}
+              />
+              <button
+                type="submit"
+                className="flex flex-c"
+                onClick={handleSubmit}
+              >
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
