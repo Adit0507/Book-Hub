af1871acf97812bf29e7bd1c580a9c574b6be5bd
Adit0507 <adi05072002@gmail.com>
2022-07-21 20:17:37 +0530

    Content and Styling changes in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <Navbar />
             <div className='header-content flex flex-c text-center text-white'>
                 <h2 className='header-title text-capitalize'>Find your book of choice.</h2><br />
-                <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
+                <p className='header-text fs-20 fw-3 text-bold'>Type in the name of a book or an author to get the results</p>
                 <SearchForm />
             </div>
         </header>
